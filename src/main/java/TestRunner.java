3120e7ab31bc5df29221773183ace5b6b652416c
AndreiSecu1997 <36158416+AndreiSecu1997@users.noreply.github.com>
2022-04-08 20:07:18 +0300

    Add first project dependencies and define the WebDriverManager

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,2 +1,15 @@
+import managers.WebDriverManager;
+
 public class TestRunner {
+    public static void main(String[] args) {
+
+        WebDriverManager fireFoxDriverManager = new WebDriverManager("firefox");
+        fireFoxDriverManager.getDriverul().get("https://www.google.com/");
+        fireFoxDriverManager.getDriverul().close();
+
+        WebDriverManager webDriverManagerObiect = new WebDriverManager("Chrome");
+        webDriverManagerObiect.getDriverul().get("https://www.google.com/");
+        webDriverManagerObiect.getDriverul().close();
+
+    }
 }
