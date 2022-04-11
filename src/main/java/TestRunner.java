f741621fd13356cfced815bd29f74bbddd2de440
AndreiSecu1997 <36158416+AndreiSecu1997@users.noreply.github.com>
2022-04-11 19:50:55 +0300

    Define the webelements for register flow and execute main actions related to them

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,15 +1,42 @@
 import managers.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class TestRunner {
-    public static void main(String[] args) {
-
-        WebDriverManager fireFoxDriverManager = new WebDriverManager("firefox");
-        fireFoxDriverManager.getDriverul().get("https://www.google.com/");
-        fireFoxDriverManager.getDriverul().close();
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager webDriverManagerObiect = new WebDriverManager("Chrome");
-        webDriverManagerObiect.getDriverul().get("https://www.google.com/");
+        webDriverManagerObiect.getDriverul().get("https://demo.opencart.com/");
+
+        WebElement myAccountIcon = webDriverManagerObiect.getDriverul().findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/i"));
+        myAccountIcon.click();
+
+        WebElement registerBtn = webDriverManagerObiect.getDriverul().findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[1]/a"));
+        registerBtn.click();
+
+        // Actiuni asupra paginii de Registration
+        WebElement firstNameInputField = webDriverManagerObiect.getDriverul().findElement(By.xpath("//*[@id=\"input-firstname\"]"));
+        WebElement lastNameInputField = webDriverManagerObiect.getDriverul().findElement(By.xpath("//*[@id=\"input-lastname\"]"));
+        WebElement emailInputField = webDriverManagerObiect.getDriverul().findElement(By.xpath("//*[@id=\"input-email\"]"));
+        WebElement phoneInputFIeld = webDriverManagerObiect.getDriverul().findElement(By.cssSelector("#input-telephone"));
+        WebElement passwordInput = webDriverManagerObiect.getDriverul().findElement(By.id("input-password"));
+        WebElement confirmPasswordInputField = webDriverManagerObiect.getDriverul().findElement(By.xpath("//*[@id=\"input-confirm\"]"));
+        WebElement continueBtn = webDriverManagerObiect.getDriverul().findElement(By.xpath("//*[@id=\"content\"]/form/div/div/input[2]"));
+
+        firstNameInputField.sendKeys("Charls");
+        lastNameInputField.sendKeys("Smith");
+        emailInputField.sendKeys("emailderas@gmail.com");
+        phoneInputFIeld.sendKeys("067717171");
+        passwordInput.sendKeys("HardPass123321");
+        confirmPasswordInputField.sendKeys("ABC");
+        confirmPasswordInputField.clear();
+        confirmPasswordInputField.sendKeys("HardPass123321");
+
+
+        continueBtn.click();
+
         webDriverManagerObiect.getDriverul().close();
+        webDriverManagerObiect.getDriverul().quit();
 
     }
 }
