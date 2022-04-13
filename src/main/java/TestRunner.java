7454ffb174a71285cf4ec1f19ce101ab11f05117
AndreiSecu1997 <36158416+AndreiSecu1997@users.noreply.github.com>
2022-04-13 20:42:06 +0300

    Add pageObject design patterns and define the register flow via new approach

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,15 +1,25 @@
 import managers.WebDriverManager;
+import pageobjects.HomePage;
+import pageobjects.RegisterPage;
+
 
 public class TestRunner {
     public static void main(String[] args) {
-
-        WebDriverManager fireFoxDriverManager = new WebDriverManager("firefox");
-        fireFoxDriverManager.getDriverul().get("https://www.google.com/");
-        fireFoxDriverManager.getDriverul().close();
-
         WebDriverManager webDriverManagerObiect = new WebDriverManager("Chrome");
+        webDriverManagerObiect.getDriverul().get("https://demo.opencart.com/");
+
+        HomePage homePage = new HomePage(webDriverManagerObiect.getDriverul());
+        homePage.navigateToRegisterPageViaHeader();
+
+        RegisterPage registerPage = new RegisterPage(webDriverManagerObiect.getDriverul());
+        registerPage.populateRegisterFormWithData("Charls", "Smith", "emailadres@gmail.com", "067717845", "HardPass123321", "HardPass123321");
+        registerPage.clickContinueBtn();
+
+        registerPage.performSearchFor("Apple");
+        registerPage.clearSearchField();
+        registerPage.performSearchFor("Samsung");
+
         webDriverManagerObiect.getDriverul().get("https://www.google.com/");
         webDriverManagerObiect.getDriverul().close();
-
     }
 }
